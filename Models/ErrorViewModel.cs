89a5d636b19b638edd94438ae6289184dccee700
Artem Koton <kotonartem@protonmail.com>
2021-11-28 17:25:19 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HistoryJeopardy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
